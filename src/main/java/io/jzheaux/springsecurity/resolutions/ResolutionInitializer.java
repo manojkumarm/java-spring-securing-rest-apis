2705f6520734ebace854d653e734ce25b13c1d7e
Manoj Reddy <manojr@wolfram.com>
2020-07-16 14:52:19 -0400

    add UserRepository dependency to initialization

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -3,20 +3,27 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
-	private final ResolutionRepository resolutions;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
-		this.resolutions = resolutions;
-	}
+  private final ResolutionRepository resolutions;
+  private final UserRepository users;
 
-	@Override
-	public void afterSingletonsInstantiated() {
-		this.resolutions.save(new Resolution("Read War and Peace", "user"));
-		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
-		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
-	}
+  public ResolutionInitializer(ResolutionRepository resolutions,
+      UserRepository users) {
+    this.resolutions = resolutions;
+    this.users = users;
+  }
+
+  @Override
+  public void afterSingletonsInstantiated() {
+    User user = new User("user",
+        "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+    this.users.save(user);
+    this.resolutions.save(new Resolution("Read War and Peace", "user"));
+    this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
+    this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+
+  }
 }
