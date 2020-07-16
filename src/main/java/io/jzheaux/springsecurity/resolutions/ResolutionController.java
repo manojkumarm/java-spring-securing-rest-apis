dfd3170eba0f949cc70504350ca3248cb65e4fa3
Manoj Reddy <manojr@wolfram.com>
2020-07-16 16:28:57 -0400

    only owner can mark tasks complete or update them

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -46,6 +46,7 @@ public class ResolutionController {
 
 	@PutMapping(path="/resolution/{id}/revise")
   @PreAuthorize("hasAuthority('resolution:write')")
+  @PostAuthorize("returnObject.orElse(null)?.owner == authentication.name")
   @Transactional
 	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
 		this.resolutions.revise(id, text);
@@ -54,6 +55,7 @@ public class ResolutionController {
 
 	@PutMapping("/resolution/{id}/complete")
   @PreAuthorize("hasAuthority('resolution:write')")
+  @PostAuthorize("returnObject.orElse(null)?.owner == authentication.name")
   @Transactional
 	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
 		this.resolutions.complete(id);
