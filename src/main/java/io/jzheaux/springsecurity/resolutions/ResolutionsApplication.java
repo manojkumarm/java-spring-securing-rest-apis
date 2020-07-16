f5b55d2262c54d66f56d00dbc5ebe36e59e1cde7
Manoj Reddy <manojr@wolfram.com>
2020-07-16 15:20:21 -0400

    enable role based access at endpoints

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -4,11 +4,23 @@ import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
-public class ResolutionsApplication {
+public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(
+        authz -> authz.mvcMatchers(HttpMethod.GET, "/resolutions", "/resolution/**")
+            .hasAuthority("resolution:read").anyRequest().hasAuthority("resolution:write"))
+        .httpBasic(httpSecurityHttpBasicConfigurer -> {
+        });
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ResolutionsApplication.class, args);
