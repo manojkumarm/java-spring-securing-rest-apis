eaae54455ae1d11484e4a40253dd2a20ac506dd9
Manoj Reddy <manojr@wolfram.com>
2020-07-16 14:58:40 -0400

    return UserDetailsService managed by JdbcUserDetailsManager

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -1,10 +1,14 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import java.util.List;
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
 public class ResolutionsApplication {
@@ -14,10 +18,12 @@ public class ResolutionsApplication {
   }
 
   @Bean
-  public UserDetailsService userDetailsService() {
-    return new InMemoryUserDetailsManager(
-        org.springframework.security.core.userdetails.User.withUsername("user")
-            .password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
-            .authorities("resolution:read").build());
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource) {
+      @Override
+      protected List<GrantedAuthority> loadUserAuthorities(String username) {
+        return AuthorityUtils.createAuthorityList("resolution:read");
+      }
+    };
   }
 }
