02a9a4fcd05b938bb1273d2e4cad7e02e254ccd4
Manoj Reddy <manojr@wolfram.com>
2020-07-16 14:42:14 -0400

    add User entity

--- src/main/java/io/jzheaux/springsecurity/resolutions/User.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/User.java
@@ -1,6 +1,53 @@
 package io.jzheaux.springsecurity.resolutions;
 
 import java.io.Serializable;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity(name = "users")
 public class User implements Serializable {
+
+  @Id
+  UUID id;
+
+  @Column
+  String username;
+
+  @Column
+  boolean enabled = true;
+
+  public User() {
+  }
+
+  public User(String username, boolean enabled) {
+    this.id = UUID.randomUUID();
+    this.username = username;
+    this.enabled = enabled;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
+  }
 }
