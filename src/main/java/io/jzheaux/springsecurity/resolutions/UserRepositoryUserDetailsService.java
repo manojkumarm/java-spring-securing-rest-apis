c7e81e6b799312f31a88c9c047077904a2ea108c
Manoj Reddy <manojr@wolfram.com>
2020-07-16 15:29:57 -0400

    setup UserDetailsService to pull in userRepository

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -6,6 +6,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserRepositoryUserDetailsService implements UserDetailsService {
 
+  private final UserRepository users;
+
+  public UserRepositoryUserDetailsService(
+      UserRepository users) {
+    this.users = users;
+  }
+
   @Override
   public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
     throw new UsernameNotFoundException("no user");
