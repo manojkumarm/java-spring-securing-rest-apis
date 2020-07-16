50f60fc3792854c05d2ababb72b2296ed9b9669a
Manoj Reddy <manojr@wolfram.com>
2020-07-16 16:31:29 -0400

    create admin user and associate with method based security

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -33,6 +33,12 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
     haswrite.grantAuthority("resolution:write");
     this.users.save(haswrite);
 
+    User admin = new User("admin","{bcrypt}$2a$10$bTu5ilpT4YILX8dOWM/05efJnoSlX4ElNnjhNopL9aPoRyUgvXAYa");
+    admin.grantAuthority("ROLE_ADMIN");
+    admin.grantAuthority("resolution:read");
+    admin.grantAuthority("resolution:write");
+    this.users.save(admin);
+
     this.resolutions.save(new Resolution("Read War and Peace", "user"));
     this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
     this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
